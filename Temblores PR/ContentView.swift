c83412eb2f37ae85c9529900b832331477682f62
Eduardo Moll <egmoll7@gmail.com>
2020-01-07 19:08:11 -0600

    Initial Commit

---
+++ Temblores PR/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Temblores PR
+//
+//  Created by Eduardo Moll on 1/7/20.
+//  Copyright © 2020 eduardo moll. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
